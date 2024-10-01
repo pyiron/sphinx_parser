bd07418bbe352129b8e0dcbab421869daf5b54cc
samwaseda <o.waseda@mpie.de>
2024-10-01 08:44:22 +0000

    extend functions

--- stinx/source/generator.py
+++ stinx/source/generator.py
@@ -6,7 +6,17 @@ indent = 4 * " "
 predefined = ["description", "default", "data_type", "required", "alias", "unit"]
 
 
-def find_alias(all_data, head=None):
+def find_alias(all_data: dict, head: list | None = None):
+    """
+    Find all aliases in the data structure.
+
+    Args:
+        all_data (dict): The data structure.
+        head (list): The current head of the data structure.
+
+    Returns:
+        dict: A dictionary with the aliases as keys and the corresponding paths as values.
+    """
     if head is None:
         head = []
     results = {}
@@ -18,26 +28,69 @@ def find_alias(all_data, head=None):
     return results
 
 
-def get(obj, path, sep="/"):
+def replace_alias(all_data: dict):
+    for key, value in find_alias(all_data).items():
+        set(all_data, key, get(all_data, value))
+    return all_data
+
+
+def get(obj: dict, path: str, sep: str = "/"):
+    """
+    Get a value from a nested dictionary.
+
+    Args:
+        obj (dict): The dictionary.
+        path (str): The path to the value.
+        sep (str): The separator.
+
+    Returns:
+        Any: The value.
+    """
     for t in path.split(sep):
         obj = obj[t]
     return obj
 
 
-def set(obj, path, value):
+def set(obj: dict, path: str, value):
+    """
+    Set a value in a nested dictionary.
+
+    Args:
+        obj (dict): The dictionary.
+        path (str): The path to the value.
+        value (Any): The value.
+    """
     *path, last = path.split("/")
     for bit in path:
         obj = obj.setdefault(bit, {})
     obj[last] = value
 
 
-def get_safe_parameter_name(name):
+def _get_safe_parameter_name(name: str):
     if keyword.iskeyword(name):
         name = name + "_"
     return name
 
 
-def get_docstring_line(data, key):
+def _get_docstring_line(data: dict, key: str):
+    """
+    Get a single line for the docstring.
+
+    Args:
+        data (dict): The data.
+        key (str): The key.
+
+    Returns:
+        str: The docstring line.
+
+    Examples:
+
+        >>> _get_docstring_line(
+        ...     {"data_type": "int", "description": "The number of iterations."},
+        ...     "iterations"
+        ... )
+        "iterations (int): The number of iterations."
+    """
     line = f"{key} ({data.get('data_type', 'dict')}): "
     if "description" in data:
         line = (line + data["description"]).strip()
@@ -59,7 +112,7 @@ def get_docstring(all_data, description=None, indent=indent, predefined=predefin
     txt += indent + "Args:\n"
     for key, data in all_data.items():
         if key not in predefined:
-            txt += 2 * indent + get_docstring_line(data, key) + "\n"
+            txt += 2 * indent + _get_docstring_line(data, key) + "\n"
     txt += indent + '"""\n'
     return txt
 
@@ -73,14 +126,14 @@ def get_input_arg(key, entry, indent=indent):
 
 
 def get_function(data, tag, predefined=predefined):
-    d = {get_safe_parameter_name(key): value for key, value in data.items()}
+    d = {_get_safe_parameter_name(key): value for key, value in data.items()}
     args = "\n".join([get_input_arg(key, value) for key, value in d.items() if key not in predefined])
     docstring = get_docstring(d, d.get("description", None))
     output = "\n".join([2 * indent + f"{key}={key}," for key in d.keys() if key not in predefined])
     return f"def get_{tag}(\n{args}\n):\n" + docstring + indent + f"return fill_values(\n{output}\n{indent})"
 
 
-def get_all_function_names(all_data, head=""):
+def get_all_function_names(all_data, head="", predefined=predefined):
     key_lst = []
     for tag, data in all_data.items():
         if tag not in predefined and data.get("data_type", "dict") == "dict":
@@ -89,7 +142,7 @@ def get_all_function_names(all_data, head=""):
     return key_lst
 
 
-def get_tags(tags, max_steps=10):
+def get_unique_tags(tags, max_steps=10):
     counter = np.ones(len(tags)).astype(int)
     for _ in range(max_steps):
         reduced_tags = ["_".join(tag.split("/")[-cc:]) for cc, tag in zip(counter, tags)]
@@ -100,3 +153,11 @@ def get_tags(tags, max_steps=10):
         for ii, tag in enumerate(reduced_tags):
             if d[tag] > 1:
                 counter[ii] += 1
+
+
+def get_all_functions(all_data):
+    tags = get_all_function_names(all_data)
+    return [
+        get_function(get(all_data, address), f_name)
+        for f_name, address in zip(get_unique_tags(tags), tags)
+    ]
