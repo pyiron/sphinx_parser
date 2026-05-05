0f05a8fbbc38f5cf601b2c65dad29ffbe5b198b5
Sam Waseda <waseda@mpie.de>
2026-05-05 13:41:34 +0200

    ruff sort

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ import shutil
 from sphinx.ext.apidoc import main
 
 import sphinx_parser
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
